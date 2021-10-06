616a6d043c445cbaaf2dec3914fba72cea0d93eb
hectoralejandrg <enero2169@gmail.com>
2021-10-05 22:09:54 -0500

    Initial commit

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'tags.apps.TagsConfig',
     'newsletters.apps.NewslettersConfig',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated',
--- newsletters/permissions.py
+++ newsletters/permissions.py
@@ -4,11 +4,6 @@ from rest_framework.permissions import BasePermission
 class NewslettersPermissions(BasePermission):
 
     def has_permission(self, request, view):
-        print(request.user.is_authenticated)
-        print(request.method)
-        print(request.user.is_staff)
-        if request.method == 'POST' and not request.user.is_authenticated or request.user.is_staff:
-            print('entra')
+        if request.method == 'POST' and not request.user.is_authenticated and not request.user.is_staff:
             return False
-        print('no entra')
         return True
